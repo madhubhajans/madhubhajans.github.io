8c62a06c87bd1dc9315f3595ccdddb4107c85232
madhubhajans <40285883+madhubhajans@users.noreply.github.com>
2024-10-09 09:43:14 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@
 (function () {
     "use strict";
     var searchBox, listElement, searchBtn, progressBar;
-    var allBhajans = p1.concat(p2, p3, p4);
+    var allBhajans = p1.concat(p2, p3, p4, p5);
     var filteredBhajans = allBhajans;
     var timeout;
     var evtdata;
